72f938f8cb5e0169a6fe55c486f257c81e0e002d
Alicia Cisneros <a01281991@gmail.com>
2018-04-11 14:21:15 -0500

    Added Camera Class

---
+++ game/VideoGame/src/videogame/Camera.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package videogame;
+
+/**
+ *
+ * @author Alicia
+ */
+public class Camera {
+    public float x; 
+    public float y; 
+    
+    public Camera(float x, float y){
+        this.x = x; 
+        this.y = y; 
+        
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void tick(Player player){
+
+    }    
+    
+}
